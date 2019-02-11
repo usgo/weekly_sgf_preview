b1751818057e4bb2c07c3e3342431d88034cdb47
Michael Hiiva <michaelhiiva@gmail.com>
2019-02-11 04:16:28 +0000

    Commented out sgfDirectory and set a default sgfDirectory to ""

--- Config.hs
+++ Config.hs
@@ -3,5 +3,7 @@ module Config where
 
 screenSpace = (155, 180)
 
+-- Uncomment and Recompile to set the configuration directory.
 -- sgfDirectory = "/var/www/usgo.org/files/weekly_problem/"
-sgfDirectory = "/var/www/dev.usgo.org/drupal/sites/default/files/weekly_problem/"
+-- sgfDirectory = "/var/www/dev.usgo.org/drupal/sites/default/files/weekly_problem/"
+sgfDirectory = ""
